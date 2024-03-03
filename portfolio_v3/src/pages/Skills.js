026c8fc930a4469675e6372ead03b4b096b8a193
Edward Yuabov <yuaboved@gmail.com>
2024-03-03 12:49:25 -0500

    changed all icons to svgs

--- portfolio_v3/src/pages/Skills.js
+++ portfolio_v3/src/pages/Skills.js
@@ -1,6 +1,64 @@
+import { HTML, CSS, JavaScript, ReactJs, Python, CPLus, NodeJs, VueJs, Flask, AWS, SQL, MongoDB } from "../icons"
 import "./Skills.css"
+import "../images/ISO_C++_Logo.svg.png"
 
 const Skills = () => {
+
+    const logo = [
+        {
+            name:"HTML",
+            icon:<HTML className="skill-img"/>
+        },
+        {
+            name:"CSS",
+            icon:<CSS className="skill-img"/>
+        },
+        {
+            name:"JavaScript",
+            icon:<JavaScript className="skill-img"/>
+        },
+        {
+            name:"React.js",
+            icon:<ReactJs className="skill-img"/>
+        },
+        {
+            name:"Python",
+            icon:<Python className="skill-img"/>
+        },
+        {
+            name:"C++",
+            icon:<CPLus className="skill-img"/>
+        },
+        {
+            name:"Node.js",
+            icon:<NodeJs className="skill-img"/>
+        },
+        {
+            name:"Vue.js",
+            icon:<VueJs className="skill-img"/>
+        },
+        {
+            name:"Flask",
+            icon:<Flask className="skill-img"/>
+        },
+        {
+            name:"Express.js",
+            icon:<NodeJs className="skill-img"/>
+        },
+        {
+            name:"AWS",
+            icon:<AWS className="skill-img"/>
+        },
+        {
+            name:"SQL",
+            icon:<SQL className="skill-img"/>
+        },
+        {
+            name:"Mongo",
+            icon:<MongoDB className="skill-img"/>
+        },
+    ]
+
     return(
         <div id="skills">
         <div>
@@ -9,117 +67,17 @@ const Skills = () => {
                 <p id="skills-subfont">WHAT I CAN DO</p>
             </div>
             <div id="skills-layout">
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img' src="https://cdn.worldvectorlogo.com/logos/html-1.svg" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">HTML</p>
-                    </div>
+                    {logo.map((skill, index) => (
+                        <div className="skill-container" key={index}>
+                            <div className="img_container">
+                                {skill.icon}
+                            </div>
+                            <div className="skill_font_container">
+                                <p className="skill">{skill.name}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img' src="https://cdn.cdnlogo.com/logos/c/18/css.svg" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">CSS</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img'
-                            src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">JavaScript</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img'
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">React.js</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img'
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">Python</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img'
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1822px-ISO_C%2B%2B_Logo.svg.png" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">C++</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img' src="https://cdn-icons-png.flaticon.com/512/5968/5968322.png" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">Node.js</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img'
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">Vue.js</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img'
-                            src="https://cdn.freebiesupply.com/logos/large/2x/flask-logo-png-transparent.png" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">Flask</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img' src="https://oauth.net/images/code/nodejs.png" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">Express.js</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img' src="https://www.svgrepo.com/show/331300/aws.svg" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">AWS</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img' src="https://www.svgrepo.com/show/331760/sql-database-generic.svg" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">SQL</p>
-                    </div>
-                </div>
-                <div className="skill-container">
-                    <div className="img_container">
-                        <img className='skill-img' src="https://cdn.cdnlogo.com/logos/m/30/mongodb-icon.svg" />
-                    </div>
-                    <div className="skill_font_container">
-                        <p className="skill">Mongo</p>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     )
