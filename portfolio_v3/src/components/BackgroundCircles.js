f907c9fc097334c20180eaed4f3f0f49475e8786
Edward Yuabov <yuaboved@gmail.com>
2024-03-19 12:52:31 -0400

    added collapse css

--- portfolio_v3/src/components/BackgroundCircles.js
+++ portfolio_v3/src/components/BackgroundCircles.js
@@ -2,11 +2,13 @@ import "./BackgroundCircles.css"
 
 const BackgroundCircles = () => {
         return(
+            // <div className="absolute">
             <>
                 <div className="slider-thumb-1"></div>
                 <div className="slider-thumb-2"></div>
                 <div className="slider-thumb-3"></div>            
-            </>
+                </>
+            // </div>
         )
 }
 
